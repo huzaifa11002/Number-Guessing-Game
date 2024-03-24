4b39d57a20831d6978ddca4dc010127ec7731f72
huzaifa11002 <huzaifahuzaifa112@gmail.com>
2024-03-25 02:39:42 +0500

    Number Guessing Game

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+// Number Guessing Game
+import inquirer from "inquirer";
+const RandomGuess = Math.floor(Math.random() * 10 + 1);
+const guessNumber = await inquirer.prompt([
+    {
+        name: "guess",
+        type: "number",
+        message: "Type your guess number (range 1-10)",
+    }
+]);
+if (RandomGuess === guessNumber.guess) {
+    console.log("You Win!");
+}
+else {
+    console.log("You Lose, Better Luck Next Time");
+    console.log(`Your Guess number is ${guessNumber.guess} and Answer is ${RandomGuess}`);
+}
